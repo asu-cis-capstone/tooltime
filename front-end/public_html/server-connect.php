0fafd8f23fa15f00acbaf87c74a56714d1f7c316
brlacquement <brlacquement@gmail.com>
2015-02-12 09:26:41 -0700

    Update server-connect.php

--- front-end/public_html/server-connect.php
+++ front-end/public_html/server-connect.php
@@ -1,10 +1,10 @@
 <?php
 	//server-connect.php
 	
-	$host 	= 'localhost';
-	$user 	= 'brlacquement';
-	$pw 	= 'Loweshats12';
-	$db		= 'BayleyConstruction';
+	$host 	= 'yourhost';
+	$user 	= 'youruser';
+	$pw 	= 'yourpassword';
+	$db		= 'yourDB';
 	
 	// Connect to the database
 	$dbc	= mysqli_connect($host, $user, $pw, $db)
