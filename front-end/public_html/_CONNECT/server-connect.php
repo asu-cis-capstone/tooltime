fab6c2eca4eac8ef7abe752a3d86a9ef208c9e33
Brandon <brlacquement@gmail.com>
2015-04-20 21:20:00 -0700

    Update server-connect.php

--- front-end/public_html/_CONNECT/server-connect.php
+++ front-end/public_html/_CONNECT/server-connect.php
@@ -1,9 +1,17 @@
 <?php
 	//server-connect.php
-	
-
-	
+	$host 	= '';
+	$user 	= '';
+	$pw 	= '';
+	$db		= '';
+	$dsn = "mysql:dbname=$db;host=$host";
 	// Connect to the database
 	$dbc	= mysqli_connect($host, $user, $pw, $db)
 				or die('Connection error! (SERVER)');
+	try {
+		$conn = new PDO($dsn, $user, $pw);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		echo 'ERROR: ' . $e->getMessage();
+	}
 ?>
